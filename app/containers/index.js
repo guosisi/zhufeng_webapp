f9d1c8f412575d69b0ee8cf4c3e35e91decc0cab
pamzer156 <1035465285@qq.com>
2017-05-26 10:35:54 +0800

    Home

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,1 +1,29 @@
 //入口
+import React,{Component} from 'react';
+//判断的路由是哪一个
+import RouterMap from '../routers';
+//关系是在这个文件中使用路由
+export default class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            loading:false
+        }
+    }
+    render(){
+        return (
+            <div>
+                {/*展示一个loading效果*/}
+                {this.state.loading?
+                    <RouterMap/>:
+                    <div>正在加载</div>
+                }
+            </div>
+        )
+    }
+    componentDidMount(){
+        this.setState({
+            loading:true
+        });
+    }
+}
--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-render(<h1>Hello world</h1>,document.getElementById('app'));
+//在这里 先渲染container下的index.js ，再在这个文件下确定渲染哪一个组件
+import App from './containers';
+//渲染App组件 App组件中渲染对应的路由
+render(<App/>,document.getElementById('app'));
