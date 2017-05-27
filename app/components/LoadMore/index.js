ac980627beafc6c65223c99137f893e4c38e5551
pamzer156 <1035465285@qq.com>
2017-05-27 11:06:09 +0800

    滚动刷新

--- app/components/LoadMore/index.js
+++ app/components/LoadMore/index.js
@@ -5,7 +5,7 @@ export default class LoadMore extends Component{
         return (
             <div className="load-more">
                 {this.props.hasMore?
-                <div onClick={this.loadMore.bind(this)}>
+                <div onClick={this.loadMore.bind(this)} ref="more">
                     点击加载更多
                 </div>:
                 <div>
@@ -14,6 +14,32 @@ export default class LoadMore extends Component{
                 }
             </div>
         )
+    }
+    componentDidMount(){
+        this.fn = () => {
+           //节流
+           clearTimeout(this.timer)
+           this.timer = setTimeout(()=>{
+               //这里是要执行的逻辑
+               //这里执行加载的方法
+               //1.如果正在加载中
+               if(this.props.isLoading){
+                   return
+               }
+               //2.如果当前加载更多的按钮漏出到页面上，加载更多
+               //当加载更多的按钮 进入到屏幕中，肯定当前按钮距离屏幕顶端的距离会小于屏幕的高度
+               let screen = window.screen.height;
+               let top = this.refs.more.getBoundingClientRect().top;//获取按钮距离屏幕的上高度
+               if(top<screen){
+                   this.props.loadMoreFn();//加载更多
+               }
+           },50)
+        };
+        window.addEventListener('scroll',this.fn)
+    }
+    componentWillUnmount(){
+        //当组件移除后 移除掉监听函数
+        window.removeEventListener('scroll',this.fn);
     }
     loadMore(){
         //正在加载中 直接return即可
